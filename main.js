0e30dfc89e73606cabbd6a9290ec0553cffbd791
sana <161457792+sanajameel1@users.noreply.github.com>
2024-04-13 05:51:38 -0700

    create to do list

---
+++ main.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+//inquirer  done
+// array    done
+//function  done
+// operations
+let todos = ["umer", "farooq"];
+async function createTodo(todos) {
+    let ans = await inquirer.prompt({
+        type: "list",
+        message: "select an operation",
+        name: "select",
+        choices: ["Add", "update", "view", "delete"],
+    });
+    if (ans.select == "Add") {
+        let addTodo = await inquirer.prompt({
+            type: "input",
+            message: "Add items in the list",
+            name: "todo",
+        });
+        todos.push(addTodo.todo);
+        console.log(todos);
+    }
+    if (ans.select == "update") {
+        let updateTodo = await inquirer.prompt({
+            type: "list",
+            message: "update items in the list",
+            name: "todo",
+            choices: todos.map(item => item)
+        });
+        let addTodo = await inquirer.prompt({
+            type: "input",
+            message: "Add items in the list",
+            name: "todo",
+        });
+        let newTodo = todos.filter(val => updateTodo.todo);
+        todos = [...newTodo, addTodo.todo];
+        console.log(todos);
+    }
+    if (ans.select == "view") {
+        console.log("*** TO DO LIST ***");
+        console.log(todos);
+        console.log("****************************");
+    }
+    if (ans.select == "delete") {
+        let deleteTodo = await inquirer.prompt({
+            type: "list",
+            message: "update items in the list",
+            name: "todo",
+            choices: todos.map(item => item)
+        });
+        let newTodo = todos.filter(val => deleteTodo.todo);
+        todos = [...newTodo];
+        console.log(todos);
+    }
+}
+createTodo(todos);
